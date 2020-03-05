172d8c5c30837666bdcabe858ce365cfe6adc8b6
Cat <ccallcottstevens@gmail.com>
2020-03-05 16:42:30 +1100

    seeded categories

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+categories = ["DIY", "camping", "kids", "hobbies", "gardening", "cooking", "furniture", "instruments"]
+
+for i in categories
+    Category.create(name: i)
+    puts "created #{i} categories"
+end
