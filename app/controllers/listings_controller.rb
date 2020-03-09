5d04a178b98f9c94880cb016ae26bb07ad89c71b
Cat <ccallcottstevens@gmail.com>
2020-03-10 10:37:24 +1100

    added previous and next month to calendar

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -5,8 +5,6 @@ class ListingsController < ApplicationController
     before_action :set_user_listing, only: [:edit, :update, :destroy]
         def index
             @listings = Listing.all
-            @search = Listing.search(params[:q])
-            @listings = @search.result
         end
     
         def show
